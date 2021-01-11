9112dbe21d506c1e9a7bb83d16c26a5a18d6de2d
Dylan Dorn <ddorn343@gmail.com>
2021-01-10 23:04:34 -0700

    Updated views
    Updated failed/success response pages

--- resources/views/loginPassed.php
+++ resources/views/loginPassed.php
@@ -1,15 +1,37 @@
-
+<!DOCTYPE html>
 <html>
-	<head>
-		<title>Login Success</title>
-	</head>
+
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+        <title>My New Job | Sign Up</title>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+        <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+        <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+        <link rel="stylesheet" href="assets/css/Login-Box-En.css">
+        <link rel="stylesheet" href="assets/css/Navigation-with-Button.css">
+        <link rel="stylesheet" href="assets/css/Pretty-Registration-Form.css">
+        <link rel="stylesheet" href="assets/css/styles.css">
+<title>Login Success</title>
+</head>
 <center>
 	<body>
-	   <h2>Login Success</h2>
-	
-		<?php
-        echo "Login was successful. Welcome!";
-        ?>		
-	</body>
-	</center>
+<nav class="navbar navbar-dark navbar-expand-lg bg-secondary navigation-clean-button" style="font-size: 14px;">
+            <div class="container"><a class="navbar-brand" href="/index">MyNewJob</a><button data-toggle="collapse"
+                    class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span
+                        class="navbar-toggler-icon"></span></button>
+            </div>
+        </nav>
+		<h2>Login Success</h2>
+        </br>
+		<a href = "profile">Go to Profile Form</a>
+			<div id="login-box-footer" style="padding:10px 20px;padding-bottom:23px;padding-top:18px;">
+             
+            </div>
+        </div>
+        <script src="assets/js/jquery.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    </body>
+
 </html>
--- routes/web.php
+++ routes/web.php
@@ -23,12 +23,19 @@ Route::post('/doLogin', 'App\Http\Controllers\LoginController@attemptLogin');
 // route for register form action
 Route::post('/doRegister', 'App\Http\Controllers\RegisterController@attemptRegister');
 
+// route for home view
 Route::get('/index', function () {
     return view('index');
 });
+// route for Login view
 Route::get('/login', function () {
     return view('login');
 });
+// route for register view
 Route::get('/signup', function () {
     return view('signup');
+});
+// route for profile view
+Route::get('/profile', function () {
+    return view('profile');
 });
